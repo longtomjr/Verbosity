a73814a5be5ddde76e349daac7a007f3ee88d73f
longtomjr <longtomjr@gmail.com>
2017-03-29 15:46:34 +0200

    Added functionality. Cannot use IO for some reason.

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,3 +1,23 @@
-for i, fluid in pairs(data.raw["fluid"]) do
-    fluid.icon = "__verbosity__/graphics/icons/fluid/water.png"
-    end
+local function layer_fluid_icon( fluid )
+  -- body
+  if fluid.icon then
+    fluid.icons = {
+      {icon = fluid.icon},
+      {icon = "__verbosity__/graphics/icons/fluid/" .. fluid.name .. ".png"}
+      }
+    fluid.icon = nil
+  elseif fluid.icons then
+    fluid.icons[#fluid.icons + 1] = {icon = "__verbosity__/graphics/icons/fluid/" .. fluid.name .. ".png"}
+  end
+end
+
+local function file_exists(name)
+   local f=io.open(name,"r")
+   if f~=nil then io.close(f) return true else return false end
+end
+
+for _, fluid in pairs(data.raw["fluid"]) do
+  if file_exists("__verbosity__/graphics/icons/fluid/" .. fluid.name .. ".png") then
+    layer_fluid_icon(fluid)
+  end
+end
