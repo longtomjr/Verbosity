ce0f46d84b3e41d85cc644c24f89ec638f2165b0
longtomjr <longtomjr@gmail.com>
2017-03-29 10:44:29 +0200

    fluids to fluid to match directories

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,3 +1,3 @@
 for i, fluid in pairs(data.raw["fluid"]) do
-    fluid.icon = "__verbosity__/graphics/icons/fluids/water.png"
+    fluid.icon = "__verbosity__/graphics/icons/fluid/water.png"
     end
