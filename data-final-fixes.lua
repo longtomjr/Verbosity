5bbefdd23e90340849123b0f12bceff700698760
longtomjr <longtomjr@gmail.com>
2017-03-29 10:43:07 +0200

    changed mod referance to lower case

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,3 +1,3 @@
 for i, fluid in pairs(data.raw["fluid"]) do
-    fluid.icon = "__Verbosity__/graphics/icons/fluids/water.png"
+    fluid.icon = "__verbosity__/graphics/icons/fluids/water.png"
     end
